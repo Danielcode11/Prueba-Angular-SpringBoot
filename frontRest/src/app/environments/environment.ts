b9353f10563d8d0557448a0aa2d97d74a2ed0b07
Daniel Serrato <danielserrator11@hotmail.com>
2024-06-15 21:38:58 -0500

    Agrego Integración y Algunos Ajustes

---
+++ frontRest/src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+// environment.ts
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8090/api/cliente/cliente/C/23445322',
+    anotherUrl: 'http://localhost:8090/api/tipo-documento' 
+  };
+  
