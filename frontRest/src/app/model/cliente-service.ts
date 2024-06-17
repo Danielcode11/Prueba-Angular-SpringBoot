ab74e74f51eac2438f2138250f685b3bb0e59d54
Daniel Serrato <danielserrator11@hotmail.com>
2024-06-17 02:52:43 -0500

    Agrego Ajustes Front

---
+++ frontRest/src/app/model/cliente-service.ts
@@ -0,0 +1,11 @@
+
+
+export interface Cliente {
+    primerNombre: string;
+    segundoNombre: string;
+    primerApellido: string;
+    segundoApellido: string;
+    telefono: string ;
+    direccion: string ;
+    ciudad: string ;
+}
